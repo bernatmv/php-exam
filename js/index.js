72937321ecf7e0f93f69441898530304c9a85970
bernatmv <bernatmv@gmail.com>
2013-12-30 10:00:54 +0100

    Added google analytics plugin

--- js/index.js
+++ js/index.js
@@ -82,6 +82,33 @@ var VERSION = "PRO";
 // load control
 var LOADED = false;
 
+// on unload
+var gaPlugin;
+function goingAway() {
+    gaPlugin.exit(function() {
+        console.debug('Analytics quit');
+    }, function() {
+        console.debug('Analytics error');
+    });
+}
+
+function permissionCallback (button) {
+    if (button === 1) {
+        // init Analytics Plugin
+        gaPlugin.init(function() {
+            console.debug('Analytics start');
+        }, function() {
+            console.debug('Analytics error');
+        }, "UA-46774704-2", 30);
+        // track start page
+        gaPlugin.trackPage( function() {
+            console.debug('Analytics: start-phpquiz.evensoftware.com');
+        }, function() {
+            console.debug('Analytics error: start-phpquiz.evensoftware.com');
+        }, "start-phpquiz.evensoftware.com");
+    }
+}
+
 // app Object
 var app = {
 
@@ -102,6 +129,9 @@ var app = {
     onDeviceReady: function() {
         if (!LOADED) {
             LOADED = true;
+            // Google Analytics Plugin
+            gaPlugin = window.plugins.gaPlugin;
+            navigator.notification.confirm('We would like your permission to collect usage data. No personal or user identifiable data will be collected.', permissionCallback, 'Attention', 'Allow,Deny');
             // default transition
             $.mobile.defaultPageTransition = 'slide';
             // transitions fallbacks
@@ -500,6 +530,7 @@ var app = {
     },
 
     goToQuestion: function (questionNumber) {
+        // build question
         $.mobile.loading("show")
         // set question as last seen
         app.setLastQuestionSeen(questionNumber);
@@ -520,6 +551,12 @@ var app = {
         // refresh
         //refreshPage();
         $("#question").trigger('create');
+        // Analytics
+        gaPlugin.trackPage( function() {
+            console.debug('Analytics: question-'+questionNumber+'-phpquiz.evensoftware.com');
+        }, function() {
+            console.debug('Analytics error: question-'+questionNumber+'-phpquiz.evensoftware.com');
+        }, "question-"+questionNumber+"-phpquiz.evensoftware.com");
     },
 
     buildQuestionButtons: function(qindex, qNum) {
